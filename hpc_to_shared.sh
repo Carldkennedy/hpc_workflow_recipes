0f4e4cb7c0902884f5d1731514c793c1214b32ab
Carl D Kennedy <116731613+Carldkennedy@users.noreply.github.com>
2023-10-03 14:05:53 +0100

    Use rsync

--- hpc_to_shared.sh
+++ hpc_to_shared.sh
@@ -4,11 +4,12 @@ user=$1
 local_path=$2
 remote_path=$3
 
-# Perform the scp (secure copy) and check if it was successful
-scp "${local_path}" "${user}@shared_storage:${remote_path}"
+# Perform the rsync and check if it was successful
+rsync -avz --progress "${local_path}" "${user}@shared_storage:${remote_path}"
 if [[ "$?" -ne 0 ]]; then
     echo "Error: Failed to copy data to shared storage."
     exit 1
 else
     echo "Data successfully copied to shared storage."
 fi
+
